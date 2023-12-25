7c65efccd7cf6f2b353e8587e1f209ac214dad28
SEONG IK HAN <tjddlr3512@pusan.ac.kr>
2023-12-25 12:40:49 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int A;
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextInt();
+
+        System.out.println(A-543);
+
+    }
+}
