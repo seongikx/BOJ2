012a9ce648beff9a769beb384bdeaa10729818db
SEONG IK HAN <tjddlr3512@pusan.ac.kr>
2023-12-25 12:40:39 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String A;
+        Scanner sc = new Scanner(System.in);
+        A = sc.next();
+
+        System.out.println(A+"??!");
+
+    }
+}
