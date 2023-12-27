112b5bb35c13cf60a6cc9b8685c571720323fce1
SEONG IK HAN <tjddlr3512@pusan.ac.kr>
2023-12-28 00:18:58 +0900

    [Bronze V] Title: A+B - 4, Time: 232 ms, Memory: 17944 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int result = 0;
+        while(sc.hasNext()){
+                int b = sc.nextInt();
+                int c = sc.nextInt();
+                result = b + c;
+                System.out.println(result);
+        }
+    }
+}
