b48ee7e90dc8a0de33a1a66818190d09b3989155
SEONG IK HAN <tjddlr3512@pusan.ac.kr>
2023-12-29 23:50:40 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 2512 ms, Memory: 159828 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String str = "";
+        Integer[] array = new Integer[n];
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<n; i++ ){
+            array[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(array, Collections.reverseOrder());
+        for(int b=0;b<n;b++){
+            sb.append(array[b]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
