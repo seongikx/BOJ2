7f23e18b56255345fa4c212d9fc87fb0481dd65d
SEONG IK HAN <tjddlr3512@pusan.ac.kr>
2024-01-17 02:46:25 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 224 ms, Memory: 17972 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -5,8 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         while(sc.hasNextLine()){
-            String str = sc.nextLine();
-            System.out.println(str);
+            System.out.println(sc.nextLine());
         }
     }
 }
