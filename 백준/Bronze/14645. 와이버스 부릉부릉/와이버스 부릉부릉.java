8c469e944fa0c601d9a63c05a45b4cc20404cda5
SEONG IK HAN <tjddlr3512@pusan.ac.kr>
2024-02-08 18:04:00 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 196 ms, Memory: 17420 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for(int i = 0 ; i < a ; i++){
+            int c = sc.nextInt();
+            int d = sc.nextInt();
+        }
+        System.out.println("비와이");
+    }
+}
