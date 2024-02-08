244a4360df88ccb6156374550295a630987d047f
SEONG IK HAN <tjddlr3512@pusan.ac.kr>
2024-02-08 19:41:22 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 196 ms, Memory: 17504 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        if(a.equals("NLCS")) System.out.println("North London Collegiate School");
+        else if(a.equals("BHA")) System.out.println("Branksome Hall Asia");
+        else if(a.equals("KIS")) System.out.println("Korea International School");
+        else if(a.equals("SJA")) System.out.println("St. Johnsbury Academy");
+
+    }
+}
