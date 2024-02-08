b042acf44012d678b7e0493bac75064e0c0a43f1
SEONG IK HAN <tjddlr3512@pusan.ac.kr>
2024-02-08 19:00:39 +0900

    [Bronze V] Title: 자동완성, Time: 196 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char a = sc.next().charAt(0);
+
+        if(a=='n'||a=='N') System.out.println("Naver D2");
+        else System.out.println("Naver Whale");
+
+    }
+}
