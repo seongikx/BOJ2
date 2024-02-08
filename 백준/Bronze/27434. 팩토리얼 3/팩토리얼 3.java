5a89dedddbc39f0195ee2ef41bd80ba81c4f824d
SEONG IK HAN <tjddlr3512@pusan.ac.kr>
2024-02-08 19:35:06 +0900

    [Bronze V] Title: 팩토리얼 3, Time: 3304 ms, Memory: 391816 KB -BaekjoonHub

---
+++ 백준/Bronze/27434. 팩토리얼 3/팩토리얼 3.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+//import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        Scanner sc = new Scanner(System.in);
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int a=Integer.valueOf(br.readLine());
+
+        System.out.println(fact(1,a));
+    }
+
+    public static BigInteger fact(int a,int n){
+        BigInteger ret=BigInteger.valueOf(a);
+
+        if(a<n){
+            int b=(a+n)/2;
+            ret=fact(a,b).multiply(fact(b+1,n)); //두개로 나눠서 풀이
+        }
+        return ret;
+    }
+}
