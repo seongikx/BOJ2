e3485350c37b29c793bad4c2767659b106a74365
SEONG IK HAN <tjddlr3512@pusan.ac.kr>
2024-02-10 22:27:28 +0900

    [Bronze V] Title: ゾロ目 (Same Numbers), Time: 204 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/27324. ゾロ目 （Same Numbers）/ゾロ目 （Same Numbers）.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        if(a.charAt(0)==a.charAt(1)) System.out.println("1");
+        else System.out.println("0");
+
+    }
+}
