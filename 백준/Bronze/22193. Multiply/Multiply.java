e003c1a5098137b0c8052e74450c8832d08a449a
SEONG IK HAN <tjddlr3512@pusan.ac.kr>
2024-02-11 00:12:47 +0900

    [Bronze V] Title: Multiply, Time: 928 ms, Memory: 59724 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22193. Multiply/Multiply.java
+++ 백준/Bronze/22193. Multiply/Multiply.java
@@ -5,6 +5,6 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         sc.nextInt();
         sc.nextInt();
-        System.out.println(sc.nextInt()*sc.nextInt());
+        System.out.println(sc.nextBigInteger().multiply(sc.nextBigInteger()));
     }
 }
